d311abea0a444ad3df1ca6b59a81fda90e1b2f4d
darioVilladaBalceiro <dariovilladabalceiro@gmail.com>
2026-09-04 21:57:06 +0200

    FuncionamientoVolante

---
+++ Assets/Scripts/LogicaConductor.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LogicaConductor : MonoBehaviour
+{
+    float acelerador;
+    float freno;
+    Vector2 giro;
+    Vector2 movimiento;
+    [SerializeField] float velocidadDeMovimiento;
+    [SerializeField] float multiplicadorGiro;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(freno);
+        Debug.Log(acelerador);
+        //Debug.Log(giro);
+        Movimiento();
+        Girar();
+    }
+    public void Acelador(InputAction.CallbackContext context)
+    {
+       acelerador= context.ReadValue<float>();
+    }
+    public void Freno(InputAction.CallbackContext context)
+    {
+        freno = context.ReadValue<float>();
+    }
+    public void Volante(InputAction.CallbackContext context)
+    {
+        giro = context.ReadValue<Vector2>();
+    }
+    void Movimiento()
+    {
+        if (acelerador<0 )
+        {
+            transform.Translate(transform.forward * velocidadDeMovimiento*Time.deltaTime);
+        }
+    }
+    void Girar()
+    {
+        //derecha x= 0.070 y=-0.07
+        //izquierda x=-0.070 y=-0.07
+
+        transform.eulerAngles = new Vector3(0, giro.x*multiplicadorGiro, 0);
+    }
+}
